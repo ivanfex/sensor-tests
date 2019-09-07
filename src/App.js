6c80a2ed555b336edea998c240d7312ee454be4b
Iván Felix <ivanfelix@Ivans-MBP.home>
2019-09-07 18:53:09 -0400

    feat(App): run sensor tests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AbsoluteOrientationSensor } from './Absolute'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.sensor = new AbsoluteOrientationSensor()
+  }
+
+  componentDidMount() {
+      
+  
+  }
+
+  initSensor = () => {
+    const options = { frequency: 1 }
+    console.log('options passed into AOS constructor:', JSON.stringify(options))
+    this.sensor = new AbsoluteOrientationSensor(options)
+    // sensor = relative ? new RelativeOrientationSensor(options) : new AbsoluteOrientationSensor(options)
+    // 'nah, AOS' , relative is false therefore we're using the Absolute Orientation Sensor
+    // sensor is NOT an array but an object that has onreading, onactivate and onerror keys
+      this.sensor.onreading = () => {
+        // console.log('within onreading:', 'sensor:', JSON.stringify(sensor))
+        // quaternion IS an array of numbers corresponding to x, y, z, w
+        console.log('within onreading:', 'quaternion:', JSON.stringify(this.sensor.quaternion))
+      }
+
+      // this.sensor.addEventListener('reading', console.log)
+      this.sensor.onerror = (event) => {
+        if (event.error.name == 'NotReadableError') {
+          console.log("Sensor is not available.")
+        }
+      }
+      this.sensor.start()
+  }
+
+  render() {
+    this.initSensor()
+    // console.log(this.sensor)
+    return (
+      <div>
+        <h1>Hey there!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
